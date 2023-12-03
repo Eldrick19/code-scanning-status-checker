38d9d14eba2cf8274f04db891808989e08744324
Eldrick19 <eldrick19@github.com>
2023-12-03 03:34:56 +0000

    repo name fix

--- run.sh
+++ run.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
+set -e
 
 TOKEN=$1
 PR_NUMBER=$2
-ORG=$3
-REPO=$4
+REPO=$3
+
+IFS='/' read -ra SPLIT_REPO <<< "$REPO"
+ORG=${SPLIT_REPO[0]}
+REPO=${SPLIT_REPO[1]}
 
 gh auth login --with-token <<< "$TOKEN"
 
-response=$(gh api graphql -f query='
-  {
-      repository(owner: "'$ORG'", name: "'$REPO'") {
-      pullRequest(number: '$PR_NUMBER') {
-        commits(last: 1) {
-          nodes {
-            commit {
-              checkSuites(first: 1, filterBy: {checkName: "CodeQL"}) {
-                nodes {
-                  checkRuns(first: 1) {
-                    nodes {
-                      name
-                      status
-                      conclusion
+get_codeql_conclusion() {
+  local response
+  response=$(gh api graphql -f query='
+    {
+        repository(owner: "'$ORG'", name: "'$REPO'") {
+        pullRequest(number: '$PR_NUMBER') {
+          commits(last: 1) {
+            nodes {
+              commit {
+                checkSuites(first: 1, filterBy: {checkName: "CodeQL"}) {
+                  nodes {
+                    checkRuns(first: 1) {
+                      nodes {
+                        name
+                        status
+                        conclusion
+                      }
                     }
                   }
                 }
@@ -30,10 +37,11 @@ response=$(gh api graphql -f query='
         }
       }
     }
-  }
-')
+  ')
+  echo $response | jq -r '.data.repository.pullRequest.commits.nodes[0].commit.checkSuites.nodes[0].checkRuns.nodes[0].conclusion'
+}
 
-conclusion=$(echo $response | jq -r '.data.repository.pullRequest.commits.nodes[0].commit.checkSuites.nodes[0].checkRuns.nodes[0].conclusion')
+conclusion=$(get_codeql_conclusion)
 if [ "$conclusion" != "SUCCESS" ]; then
   echo "CodeQL check failed"
   exit 1
